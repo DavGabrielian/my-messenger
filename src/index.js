040d06a55fae16f340ee2a30e0b75b13821fb4e8
David Gabrielian <davidgabrielofficial@gamil.com>
2020-07-23 16:10:05 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebase = require("firebase");
+require("firebase/firestore");
+firebase.initializeApp({
+  apiKey: "AIzaSyB3O6WaN0jRcs7fqhDFDrejXETodTx3kZk",
+  authDomain: "my-messenger-99c12.firebaseapp.com",
+  databaseURL: "https://my-messenger-99c12.firebaseio.com",
+  projectId: "my-messenger-99c12",
+  storageBucket: "my-messenger-99c12.appspot.com",
+  messagingSenderId: "595226245835",
+  appId: "1:595226245835:web:3fda36e765cd59a72a794e",
+  measurementId: "G-L80MXLN9HF",
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<div>hello world</div>, document.getElementById("root"));
+
 serviceWorker.unregister();
