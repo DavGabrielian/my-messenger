faaf7c7c39a59a93c91bf8c49ea9d650c92c4545
David Gabrielian <davidgabrielofficial@gamil.com>
2020-07-25 14:22:38 +0400

    '1.3.5'

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import ChatList from "../chatList/chatList";
-export default class Dashboard extends Component {
+
+const firebase = require("firebase");
+
+class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
@@ -14,12 +17,48 @@ export default class Dashboard extends Component {
     return (
       <div>
         <div>hello from dashboard</div>
-        <ChatList history={this.props.history}></ChatList>
+        <ChatList
+          history={this.props.history}
+          newChatBtnFn={this.newChatBtnClicked}
+          selectedChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}
+        ></ChatList>
       </div>
     );
   }
 
+  selectChat = (chatIndex) => {
+    console.log("selectedChat", chatIndex);
+  };
 
+  newChatBtnClicked = () => {
+    this.setState({
+      newChatFormVisible: true,
+      selectChat: null,
+    });
+  };
 
-  
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async (_usr) => {
+      if (!_usr) this.props.history.push("/login");
+      else {
+        await firebase
+          .firestore()
+          .collection("chats")
+          .where("users", "array-contains", _usr.email)
+          .onSnapshot(async (res) => {
+            const chats = res.docs.map((_doc) => _doc.data());
+            await this.setState({
+              email: _usr.email,
+              chats: chats,
+            });
+            console.log(this.state);
+          });
+      }
+    });
+  };
 }
+
+export default Dashboard;
