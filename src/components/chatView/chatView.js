ac57834839f2ad562f5fd002ce85ed1b8f79e0f0
David Gabrielian <davidgabrielofficial@gamil.com>
2020-07-26 16:47:45 +0400

    '1.4.9'

--- src/components/chatView/chatView.js
+++ src/components/chatView/chatView.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
-import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
 
 class ChatView extends Component {
   render() {
-    const { classes } = this.props;
-
-    return <div className={classes.content}>hello from chatview</div>;
+    const { classes, chat, user } = this.props;
+    if (chat === undefined) {
+      return <main className={classes.content}></main>;
+    } else {
+      return (
+        <div>
+          <div className={classes.chatHeader}>
+            {chat.users.filter((_usr) => _usr !== user)}
+          </div>
+          <main className={classes.content}>
+            {chat.messages.map((_msg, _index) => {
+              return (
+                <div
+                  key={_index}
+                  className={
+                    _msg.sender === user ? classes.userSent : classes.friendSent
+                  }
+                >
+                  {_msg.message}
+                </div>
+              );
+            })}
+          </main>
+        </div>
+      );
+    }
   }
 }
 
--- src/components/chatView/styles.js
+++ src/components/chatView/styles.js
@@ -1,56 +1,54 @@
-const styles = theme => ({
+const styles = (theme) => ({
+  content: {
+    height: "calc(100vh - 100px)",
+    overflow: "auto",
+    padding: "25px",
+    marginLeft: "300px",
+    boxSizing: "border-box",
+    overflowY: "scroll",
+    top: "50px",
+    width: "calc(100% - 300px)",
+    position: "absolute",
+  },
 
-    content: {
-      height: 'calc(100vh - 100px)',
-      overflow: 'auto',
-      padding: '25px',
-      marginLeft: '300px',
-      boxSizing: 'border-box',
-      overflowY: 'scroll',
-      top: '50px',
-      width: 'calc(100% - 300px)',
-      position: 'absolute'
-    },
-  
-    userSent: {
-      float: 'left',
-      clear: 'both',
-      padding: '20px',
-      boxSizing: 'border-box',
-      wordWrap: 'break-word',
-      marginTop: '10px',
-      backgroundColor: '#707BC4',
-      color: 'white',
-      width: '300px',
-      borderRadius: '10px'
-    },
-  
-    friendSent: {
-      float: 'right',
-      clear: 'both',
-      padding: '20px',
-      boxSizing: 'border-box',
-      wordWrap: 'break-word',
-      marginTop: '10px',
-      backgroundColor: '#707BC4',
-      color: 'white',
-      width: '300px',
-      borderRadius: '10px'
-    },
-  
-    chatHeader: {
-      width: 'calc(100% - 301px)',
-      height: '50px',
-      backgroundColor: '#344195',
-      position: 'fixed',
-      marginLeft: '301px',
-      fontSize: '18px',
-      textAlign: 'center',
-      color: 'white',
-      paddingTop: '10px',
-      boxSizing: 'border-box'
-    }
-  
-  });
-  
-  export default styles;
+  userSent: {
+    float: "left",
+    clear: "both",
+    padding: "20px",
+    boxSizing: "border-box",
+    wordWrap: "break-word",
+    marginTop: "10px",
+    backgroundColor: "#2E86D9",
+    color: "white",
+    width: "300px",
+    borderRadius: "10px",
+  },
+
+  friendSent: {
+    float: "right",
+    clear: "both",
+    padding: "20px",
+    boxSizing: "border-box",
+    wordWrap: "break-word",
+    marginTop: "10px",
+    backgroundColor: "#2E86D9",
+    color: "white",
+    width: "300px",
+    borderRadius: "10px",
+  },
+
+  chatHeader: {
+    width: "calc(100% - 301px)",
+    height: "50px",
+    backgroundColor: "#BDC3C7",
+    position: "fixed",
+    marginLeft: "301px",
+    fontSize: "18px",
+    textAlign: "center",
+    color: "black",
+    paddingTop: "10px",
+    boxSizing: "border-box",
+  },
+});
+
+export default styles;
